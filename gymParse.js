d780ff31ffec3d27aa07054a1c2aed946c3306b0
Daniel Berlin <=>
2024-06-25 12:36:01 -0400

    working

--- gymParse.js
+++ gymParse.js
@@ -1,14 +1,52 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const filepath = './gymRecords.txt';
+var oGym = {};
 
-async function getText(filepath) {
-    fs.readFile(filepath, 'utf8', (err, data) => {
-        if (err) {
-            return err;
-        } 
+async function readFileAsync(filePath) {
+    try {
+        const data = await fs.readFile(filePath, 'utf8');
         return data;
-    })
+    } catch (err) {
+        console.error('Error reading file:', err);
+    }
 }
 
-var text = await(getText(filepath));
+(async () => {
+    oGym['raw'] = await readFileAsync(filepath);
 
+    oGym['raw'] = inputFormat(oGym['raw'])
+
+    getDates(oGym);
+    getWorkouts(oGym);
+
+})();   
+
+
+
+
+function getDates(oGym) {
+    var text = oGym['raw'];
+    var dateReg = new RegExp(/20\d{6}/, 'g');
+    oGym['dates'] = text.match(dateReg);
+    console.dir(oGym['dates']);
+}
+
+function getWorkouts(oGym) {
+    var text = oGym['raw'];
+    var workoutReg = new RegExp(/(?<=\d{8}\s).*(?=\s20\d{6})/, 'g');
+    oGym['workouts'] = text.match(workoutReg);
+    console.dir(oGym['workouts']);
+}
+
+function inputFormat(string) {
+    string = string.replaceAll('\n', ' ');
+    string = string.replaceAll('\r', '');
+    string = gapReplacer(string);
+
+    return string;
+}
+
+function gapReplacer(string) {
+    string = string.replaceAll(/\s{2,}/g, ' ');
+    return string;
+}
