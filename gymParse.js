684081223739d1f2497dd77d431bfd87745127ca
Daniel Berlin <=>
2024-06-29 15:47:20 -0400

    redoing

--- gymParse.js
+++ gymParse.js
@@ -1,7 +1,9 @@
 const fs = require('fs').promises;
 const filepath = './gymRecords.txt';
 
-var oGym = {};
+var oGym = {
+    'dates': {},
+};
 
 async function readFileAsync(filePath) {
     try {
@@ -14,81 +16,48 @@ async function readFileAsync(filePath) {
 
 (async () => {
     oGym['raw'] = await readFileAsync(filepath);
-
     getStarted(oGym);
-
-    // console.dir(oGym);
-
-    // console.log(oGym['dates'].length);
-    // console.log(oGym['workouts'].length);
-
 })();   
 
 
 
-
-
-
-
-//* MOTHER FUNCTIONS
 function getStarted(oGym) {
-    oGym['raw'] = inputFormat(oGym['raw'])
-    getDates(oGym); // X-Axis
-    getWorkouts(oGym);
+    oGym['textArr'] = breakdown(oGym['raw']);
+    parse(oGym);
+    logStats(oGym);
 }
 
+function breakdown(string) {
+    var sub = 'breakdown()';
+    console.log(sub, '>>>>>', 'has been called');
 
-//* PRE-PARSING
-function inputFormat(string) {
-    // string = string.replaceAll('\n', '');
-    // string = string.replaceAll('\r', '+');
-    // string = gapReplacer(string);
-
-    // string = string.replaceAll(/(20\d{6})/g, '\r\n$1\n');
-    // string = string.replaceAll(/(lbs)/g, ' $1\n');
-    // string = string.replaceAll(/(x\d{1,})(\D)/g, '$1\n$2');
-    // string = string.replaceAll(/(\s*\d{1,}lb\s)/g, '$1');
-
-
-    console.log(string)
-    return string;
+    var arr = string.split('\r\n');
+    arr = arr.filter(el => el !== '');
+    console.log(sub, '>>>', 'output array\n', arr);
+    return arr;
 }
 
+function parse(oGym) {
+    var sub = 'parse()';
+    console.log(sub, '>>>>>', 'has been called');
 
-//* PARSING
-// X-Axis
-function getDates(oGym) {
+    oGym['textArr'].forEach((el, i, a) => {
+        if (el.match(/^20\d{6}$/)) {
+            console.log(sub, '>>>', 'date code found', el);
+            oGym['dates'][el] = {};
+            oGym['dates'][el].readable = ''; //! FORMAT 
+            oGym['dates'][el].excercizes = {};
+            var excersize = a[i+1]; //? how to do more than one
+        }
+    });
+}
 
-    const aMonths = '0 January February March April May June July August September October November December'.split(' ');
-
-    console.group('\nDates / X-Axis');
-    var text = oGym['raw'];
-    var dateReg = new RegExp(/20\d{6}/, 'g');
-    oGym['dates'] = text.match(dateReg);
-    
-    
-    oGym['formatted dates'] = oGym['dates'].map(date => {
-        var iMonth = parseInt(date.substring(4, 6));
-        var month = aMonths[iMonth].substring(0, 3);
-        var day = date.substring(6, 8);
-        var year = date.substring(2, 4)
-        return `${month}-${day} '${year}`;
-    })
-    console.dir(oGym['formatted dates']);
-    
+function logStats(oGym) {
+    var sub = 'logStats()';
+    console.log(sub, '>>>>>', 'has been called');
+    console.group('\n+++ GYM STATS +++');
+    var numWorkouts = oGym['dates'].length;
+    console.log('### total number of workouts: ' + numWorkouts)
     console.groupEnd();
 }
 
-function getWorkouts(oGym) {
-    var text = oGym['raw'];
-    var workoutReg = new RegExp(/(?<=\d{8}\s).*(?=\s20\d{6})/, 'g');
-    oGym['workouts'] = text.match(workoutReg);
-    // console.dir(oGym['workouts']);
-}
-
-
-//* GENERAL FUNCTIONS
-function gapReplacer(string) {
-    string = string.replaceAll(/\s{2,}/g, ' ');
-    return string;
-}
