9a65c3538a83d3b5a062cec4d6839ed083429d13
Daniel Berlin <daniel.berlin@barcoding.com>
2024-07-05 16:12:55 -0400

    node data parsing complete

--- gymParse.js
+++ gymParse.js
@@ -1,39 +1,137 @@
+const { totalmem } = require('os');
+const { exitCode } = require('process');
+
 const fs = require('fs').promises;
 const filepath = './gymRecords.txt';
 
+const util = require('util');
+
 var oGym = {
+    'dateCodes': [],
     'dates': {},
 };
 
 async function readFileAsync(filePath) {
+    var sub = 'readFileAsync()';
+    console.log('\n' + sub, '>>>>>', 'has been called');
+
     try {
         const data = await fs.readFile(filePath, 'utf8');
+        var nChars = data.length;
+        console.log(sub, '>>>', 'File read');
+        console.log(sub, '>>>', 'total characters: ' + nChars);
         return data;
     } catch (err) {
-        console.error('Error reading file:', err);
+        console.error('!!!!!!!!!! Error reading file:', err);
     }
 }
 
 (async () => {
     oGym['raw'] = await readFileAsync(filepath);
     getStarted(oGym);
+
+    console.log(util.inspect(oGym.dates, { depth: null, colors: true }));        
 })();   
 
 
 
+
+
+
+
+
+
 function getStarted(oGym) {
-    oGym['textArr'] = breakdown(oGym['raw']);
+    var sub = 'getStarted()';
+    console.log(sub, '>>>>>', 'has been called');
+
+    oGym['formatted'] = format(oGym['raw'])
+    oGym['textArr'] = breakdown(oGym['formatted']);
     parse(oGym);
+    totalWeights(oGym);
     logStats(oGym);
+}
+
+function format(string) {
+    var sub = 'format()';
+    console.log(sub, '>>>>>', 'has been called');
+
+    string = string.toLowerCase();
+
+    string = string.replaceAll(/\blat\b/g, 'lateral');
+    string = string.replaceAll(/\bpull\sdown\b/g, 'pulldown');
+    string = string.replaceAll(/(\bunloaded\s\d+lbs\b)/g, '($1)');
+
+    return string;
+
+}
+
+function totalWeights(oGym) {
+    var sub = 'totalWeights()';
+    // console.log(sub, '>>>>>', 'has been called');
+
+    var dates = Object.keys(oGym['dates']);
+
+    dates.forEach(date => {
+        var exercises = Object.keys(oGym['dates'][date]['exercises']);
+        exercises.forEach(exercise => {
+            var exe = oGym['dates'][date]['exercises'][exercise];
+            var weightReps = repAdder(exe['sets']);
+            exe['totalReps'] = weightReps.totalReps;
+            // Y-Axis
+            exe['totalWeightReps'] = weightReps.totalWeightReps;
+        })
+    });
+}
+
+function repAdder(sets) {
+    var sub = 'repAdder()';
+    // console.log(sub, '>>>>>', 'has been called');
+
+    var weightObject = {
+        'totalReps': 0,
+        'totalWeightReps': 0
+    }
+
+    var weights = [], reps = [];
+    sets.forEach(set => {
+        set = set.split(' ');
+        weights.push(set[0]);
+        reps.push(set[1]);
+    });
+
+    weights = weights.map(weight => {
+        return weight.match(/^\d+/)[0];
+    })
+
+    reps = reps.map(rep => {
+        rep = rep.split('x');
+        var nSets = parseInt(rep[0]);
+        var nReps = parseInt(rep[1]);
+        return nSets*nReps;
+    })
+
+    var weightRepsArr = reps.map((total, i, a) => {
+        return parseInt(weights[i])*total;
+    })
+
+    var totalWeightReps = weightRepsArr.reduce((a,b) => a+b)
+
+    var totalReps = reps.reduce((a,b) => a+b);
+
+    weightObject['totalReps'] = totalReps;
+    weightObject['totalWeightReps'] = totalWeightReps;
+
+    return weightObject;
 }
 
 function breakdown(string) {
     var sub = 'breakdown()';
     console.log(sub, '>>>>>', 'has been called');
 
-    var arr = string.split('\r\n');
+    var arr = string.split('\n');
     arr = arr.filter(el => el !== '');
-    console.log(sub, '>>>', 'output array\n', arr);
+    // console.log(sub, '>>>', 'output array\n', arr);
     return arr;
 }
 
@@ -41,23 +139,83 @@ function parse(oGym) {
     var sub = 'parse()';
     console.log(sub, '>>>>>', 'has been called');
 
+    var currentDate, currentExcercise;
+
     oGym['textArr'].forEach((el, i, a) => {
+
+        el = el.trim();
+        el = gapReplacer(el);
+
+        // date
         if (el.match(/^20\d{6}$/)) {
-            console.log(sub, '>>>', 'date code found', el);
-            oGym['dates'][el] = {};
-            oGym['dates'][el].readable = ''; //! FORMAT 
-            oGym['dates'][el].excercizes = {};
-            var excersize = a[i+1]; //? how to do more than one
+            console.log('\n' + sub, '>>>', 'date code found', el);
+            var dateCode = el.trim();
+            oGym['dateCodes'].push(dateCode);
+
+            currentDate = dateCode;
+
+            oGym['dates'][dateCode] = {};
+            oGym['dates'][dateCode]['readable'] = dateReadable(dateCode);
+            oGym['dates'][dateCode]['exercises'] = {};
+        }
+
+        // excercise
+        if (el.match(/^[a-zA-Z]+/, 'i')) {
+            var exercise = el.trim(); 
+            currentExcercise = exercise;
+            oGym['dates'][currentDate]['exercises'][exercise] = {};
+            oGym['dates'][currentDate]['exercises'][exercise].sets = [];
+        } 
+
+        // reps
+        if (el.match(/^\d+lb/)) {
+            oGym['dates'][currentDate]['exercises'][currentExcercise].sets.push(el);
         }
     });
 }
 
-function logStats(oGym) {
-    var sub = 'logStats()';
-    console.log(sub, '>>>>>', 'has been called');
-    console.group('\n+++ GYM STATS +++');
-    var numWorkouts = oGym['dates'].length;
-    console.log('### total number of workouts: ' + numWorkouts)
-    console.groupEnd();
+function dateReadable(date) {
+    var sub = 'dateReadable()';
+    // console.log(sub, '>>>>>', 'has been called');
+    
+    const aMonths = '0 January February March April May June July August September October November December'.split(' ');
+    
+    var iMonth = parseInt(date.substring(4, 6));
+    var month = aMonths[iMonth].substring(0, 3);
+    var day = date.substring(6, 8);
+    var year = date.substring(2, 4);
+    
+    var hrDate = `${month}-${day} '${year}`;
+    console.log(sub, '>>>', 'human readable date: ' + hrDate);
+    
+    return hrDate;
 }
 
+
+//* GENERAL FUNCTIONS
+function gapReplacer(string) {
+    string = string.replaceAll(/\s{2,}/g, ' ');
+    return string;
+}
+
+
+//* LOGGING FUNCTIONS
+function logStats(oGym) {
+    var sub = 'logStats()';
+    // console.log(sub, '>>>>>', 'has been called');
+
+    console.group('\n+++ GYM STATS +++');
+
+    var numWorkouts = oGym['dateCodes'].length;
+
+    var exercises = [];
+    Object.keys(oGym['dates']).forEach(date => {
+        var exes = Object.keys(oGym['dates'][date]['exercises']);
+        exercises.push(exes);
+    })
+    exercises = new Set(exercises.flat());
+
+    console.log('### total number of workouts: ' + numWorkouts);
+    console.log('### total number of unique exercises: ' + exercises.size);
+    console.groupEnd();
+}
